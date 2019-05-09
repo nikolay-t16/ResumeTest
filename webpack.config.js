c514d308e8bf1fe5ef0c4835e14b120813adbb61
Nikolay Tereshchenko <nikolay.ter@gmail.com>
2019-05-09 23:58:33 +0300

    Настрока webpack для pug

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const pug = {
+	test: /\.pug$/,
+	use: ['html-loader?attrs=false', 'pug-html-loader']
+};
+
+const config = {
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].bundle.js'
+	},
+	module: {
+		rules: [pug]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: 'src/index.pug',
+			inject: false
+		})
+	]
+};
+
+module.exports = config;
