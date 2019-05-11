2062e4dbcfa4141819f40ee1671376bd3119ff1e
Nikolay Tereshchenko <nikolay.ter@gmail.com>
2019-05-12 00:34:38 +0300

    scss в css

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,24 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const pug = {
 	test: /\.pug$/,
 	use: ['html-loader?attrs=false', 'pug-html-loader']
+};
+
+const js = {
+	test: /\.js$/,
+	use: {
+		loader: "babel-loader",
+		options: {presets: ["es2015"]}
+	}
+};
+
+const scss = {
+	test: /\.scss$/,
+	use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
 };
 
 const config = {
@@ -14,13 +28,16 @@ const config = {
 		filename: '[name].bundle.js'
 	},
 	module: {
-		rules: [pug]
+		rules: [pug, js, scss]
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
 			filename: 'index.html',
 			template: 'src/index.pug',
 			inject: false
+		}),
+		new MiniCssExtractPlugin({
+			template: 'src/index.scss',
 		})
 	]
 };
