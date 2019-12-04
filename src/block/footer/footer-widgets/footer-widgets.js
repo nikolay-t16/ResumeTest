b4faf5b0a668598264169d243e0fa0eb6d6a5925
Nikolay Tereshchenko <nikolay.ter@gmail.com>
2019-12-04 18:39:06 +0300

    Рефакторинг кода по примеру https://github.com/fullstack-development/flat-starter-kit

--- src/block/footer/footer-widgets/footer-widgets.js
+++ src/block/footer/footer-widgets/footer-widgets.js
@@ -1,3 +1,4 @@
 import './footer-widgets.scss'
 import '../../logo/logo'
 import '../footer-social/footer-social'
+import '../../forms/input/input'
--- src/block/forms/range-slider/range-slider.js
+++ src/block/forms/range-slider/range-slider.js
@@ -2,6 +2,18 @@ import 'ion-rangeslider/js/ion.rangeSlider.min';
 import './range-slider.scss';
 import './range-slider-ion.scss';
 
-$(function(){
-  $(".range-slider__slider-input").ionRangeSlider();
-});
+$(function () {
+  $('.range-slider').each(function () {
+    const slider = $(this).find('.range-slider__slider-input');
+    if(slider.length) {
+      const fromLabel = $(this).find('.range-slider__header-value-min');
+      const toLabel = $(this).find('.range-slider__header-value-max');
+      $(slider).ionRangeSlider({
+        onChange: function (data) {
+          fromLabel.html(data.from_pretty);
+          toLabel.html(data.to_pretty);
+        }
+      });
+    }
+  });
+})
