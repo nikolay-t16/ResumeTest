ddb9570a4b76afcb3b64c3e8194ea1baf2a12f98
Nikolay Tereshchenko <nikolay.ter@gmail.com>
2019-12-13 18:01:17 +0300

    form_elements: dropdown выпадающий список

--- src/block/forms/dropdown/dropdown.js
+++ src/block/forms/dropdown/dropdown.js
@@ -1,24 +1,39 @@
-import './dropdown.scss';
+import './dropdown.scss'
 
-$(function() {
-  $('.dropdown').each(function() {
-    const dropdownItems = $(this).find('.dropdown__items');
-    $(this).find('.dropdown__input').click(function() {
-      const obj = $(this);
-      const icon = obj.find('.dropdown__icon');
-      const isClose =  icon.text() === 'expand_more';
+$(function () {
+  $('.dropdown').each(function () {
+    const dropdownItems = $(this).find('.dropdown__items')
+    $(this).find('.dropdown__input').each(function () {
+      $(this).click(function () {
+        const obj = $(this)
+        const isClose = obj.hasClass('dropdown_state-open')
 
-      if (isClose) {
-        obj.addClass('dropdown_state-open');
-      }
-
-      dropdownItems.slideToggle(400, () => {
-        if (!isClose) {
-          obj.removeClass('dropdown_state-open');
+        if (isClose) {
+          obj.addClass('dropdown_state-open')
         }
-        icon.text(isClose ? 'expand_less' : 'expand_more');
-      });
-    });
-  });
 
-});
+        dropdownItems.slideToggle(400, () => {
+          if (!isClose) {
+            obj.removeClass('dropdown_state-open')
+          }
+        })
+      })
+    })
+    $(this).find('.dropdown__item').each(function () {
+      const value = $(this).find('.dropdown__item-control-value')
+      const addBtn = $(this).find('.dropdown__item-control-add')
+      const removeBtn = $(this).find('.dropdown__item-control-remove')
+
+      addBtn.click(function () {
+        value.html(+value.html() + 1)
+      })
+
+      removeBtn.click(function () {
+        const val = +value.html()
+        value.html(val === 0 ? 0 : val - 1)
+      })
+    })
+
+  })
+
+})
