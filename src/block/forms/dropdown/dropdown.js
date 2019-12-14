ef4303e4565e5930eb3f626d1c2d7458d7e3fd4f
Nikolay Tereshchenko <nikolay.ter@gmail.com>
2019-12-14 18:29:07 +0300

    Черновой вариант dropdown без кнопок и с разбивкой значения.

--- src/block/forms/dropdown/dropdown.js
+++ src/block/forms/dropdown/dropdown.js
@@ -1,39 +1,97 @@
 import './dropdown.scss'
 
-$(function () {
-  $('.dropdown').each(function () {
-    const dropdownItems = $(this).find('.dropdown__items')
-    $(this).find('.dropdown__input').each(function () {
-      $(this).click(function () {
-        const obj = $(this)
-        const isClose = obj.hasClass('dropdown_state-open')
+function getItemValue (count, labelDeclensions) {
 
-        if (isClose) {
-          obj.addClass('dropdown_state-open')
+  if (labelDeclensions.length < 3 || count < 0) {
+    return '';
+  }
+  if (
+    [0, 5, 6, 7, 8, 9].indexOf(count % 10) >= 0
+    || [11, 12, 13, 14].indexOf(count % 100) >= 0
+  ) {
+    return `${count} ${labelDeclensions[0]}`;
+  }
+  if (count % 10 === 1) {
+    return `${count} ${labelDeclensions[1]}`;
+  }
+  if ([2, 3, 4].indexOf(count % 10) >= 0) {
+    return `${count} ${labelDeclensions[2]}`;
+  }
+}
+function getDropdownItemsData (dropdown) {
+  const items = [];
+  $(dropdown).find('.dropdown__item').each(function() {
+    const value = +$(this).find('.dropdown__item-control-value').text();
+    const labelDeclensions = $(this).data('label_declensions') || [];
+    const item = {
+      value,
+      labelDeclensions,
+    };
+    items.push(item);
+  });
+  return items;
+}
+
+function setMultiValue(dropdown) {
+  const dropdownLabelTag = dropdown.find('.dropdown__label');
+  const placeholder = dropdownLabelTag.data('placeholder');
+  const itemsData = getDropdownItemsData(dropdown);
+  let label = '';
+  const count = itemsData.length > 2 ? 2 : itemsData.length;
+  for (let i = 0; i < count; i++) {
+    const item = itemsData[i];
+    if (label.length > 0) {
+      label += ', ';
+    }
+    const itemLabel = getItemValue(item.value, item.labelDeclensions);
+    label += itemLabel;
+  }
+  if (itemsData.length > 2) {
+    label += '...';
+  }
+  const valueTag =  dropdownLabelTag.find('.dropdown__value');
+  const newValue = label === '' ? placeholder: label;
+  valueTag.text(newValue);
+
+}
+
+$(function() {
+  $('.dropdown').each(function() {
+    const dropdown = $(this);
+    const dropdownItems = $(this).find('.dropdown__items');
+    const dropdownLabelTag = $(this).find('.dropdown__label');
+    dropdownLabelTag.click(function() {
+      const isOpen = $(this).hasClass('dropdown_state-open');
+      if (!isOpen) {
+        $(this).addClass('dropdown_state-open');
+      }
+      dropdownItems.slideToggle(400, () => {
+        if (isOpen) {
+          $(this).removeClass('dropdown_state-open');
         }
+      });
+    });
+    $(this).find('.dropdown__item').each(function() {
 
-        dropdownItems.slideToggle(400, () => {
-          if (!isClose) {
-            obj.removeClass('dropdown_state-open')
-          }
-        })
-      })
-    })
-    $(this).find('.dropdown__item').each(function () {
-      const value = $(this).find('.dropdown__item-control-value')
-      const addBtn = $(this).find('.dropdown__item-control-add')
-      const removeBtn = $(this).find('.dropdown__item-control-remove')
+      const valueTag = $(this).find('.dropdown__item-control-value');
+      const addBtn = $(this).find('.dropdown__item-control-add');
+      const removeBtn = $(this).find('.dropdown__item-control-remove');
+      addBtn.click(function() {
+        valueTag.html(+valueTag.html() + 1);
+        setMultiValue(dropdown);
+      });
 
-      addBtn.click(function () {
-        value.html(+value.html() + 1)
-      })
+      removeBtn.click(function() {
+        const val = +valueTag.html();
+        if (val !== 0) {
+          valueTag.html(val === 0 ? 0 : val - 1);
+          setMultiValue(dropdown);
+        }
+      });
 
-      removeBtn.click(function () {
-        const val = +value.html()
-        value.html(val === 0 ? 0 : val - 1)
-      })
-    })
+    });
+    setMultiValue(dropdown);
 
-  })
+  });
 
-})
+});
